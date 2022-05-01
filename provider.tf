62287c6b2b74cba49616e72db7b1dc9445c62beb
Balajigit2019 <47142716+Balajigit2019@users.noreply.github.com>
2022-05-01 23:51:29 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
 #  shared_credentials_file = "c:/Users/bvijay062/.aws/credentials" # old version syntax and its depreciated
-  shared_credentials_files = ["c:/Users/bvijay062/.aws/credentials"] # latest syntax
+  shared_credentials_files = ["credentials"] # latest syntax
   region = "ap-south-1"
 }
