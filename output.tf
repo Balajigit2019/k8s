246909c990b8df839c189691f35c5422cb7b2434
Balajigit2019 <47142716+Balajigit2019@users.noreply.github.com>
2022-05-04 18:41:39 +0530

    Update output.tf

--- output.tf
+++ output.tf
@@ -2,5 +2,5 @@ output "k8s_master_public_ip" {
   value = aws_instance.jenkins.public_ip
 }
 output "public_subnet_id" {
-  value = aws_subnet.pub_sub.public_subnet_id
+  value = aws_subnet.pub_sub.id
 }   
